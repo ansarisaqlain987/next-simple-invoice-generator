d6997d1730a1ca684b1941be0ea155a8166b3f44
Saqlain Ansari <ansarisaqlain987@gmail.com>
2024-07-29 16:36:20 +0530

    aded sorting button

--- src/app/dashboard/clients/columns.tsx
+++ src/app/dashboard/clients/columns.tsx
@@ -7,33 +7,35 @@ import { ArrowUpDown } from "lucide-react"
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 export type Payment = {
-  id: string
-  amount: number
-  status: "pending" | "processing" | "success" | "failed"
-  email: string
+    id: string
+    amount: number
+    status: "pending" | "processing" | "success" | "failed"
+    email: string
 }
 
 export const columns: ColumnDef<Payment>[] = [
-  {
-    accessorKey: "status",
-    header: "Status",
-  },
-  {
-    accessorKey: "email",
-    header: ({ column }) => {
-        return (
-          <Button
-            variant="ghost"
-            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-          >
-            Email
-            <ArrowUpDown className="ml-2 h-4 w-4" />
-          </Button>
-        )
-      },
-  },
-  {
-    accessorKey: "amount",
-    header: "Amount",
-  },
+    {
+        accessorKey: "status",
+        header: "Status",
+    },
+    {
+        accessorKey: "email",
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                    className="pl-0"
+                >
+
+                    <ArrowUpDown className="h-4 w-4 mr-2" />
+                    Email
+                </Button>
+            )
+        },
+    },
+    {
+        accessorKey: "amount",
+        header: "Amount",
+    },
 ]
